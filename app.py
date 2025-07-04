65a17dc70575456c561fd81ccd455902dba3992b
Soroush Mosahebfard <s.mosahebfard@aol.com>
2025-07-04 11:31:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, send_from_directory
 app = Flask(__name__, static_folder='templates')
 @app.route('/')
-def index(): return send_from_directory('templates','index.html')
+def index():
+    return send_from_directory('templates','index.html')
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
