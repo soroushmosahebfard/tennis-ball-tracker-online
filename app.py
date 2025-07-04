ab7609453f303aac17bd3b83e557988041678c78
Soroush Mosahebfard <s.mosahebfard@aol.com>
2025-07-04 11:20:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
-from flask import Flask, render_template
-import os
-
-app = Flask(__name__)
-
+from flask import Flask, send_from_directory
+app = Flask(__name__, static_folder='templates')
 @app.route('/')
-def index():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+def index(): return send_from_directory('templates','index.html')
+if __name__=='__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
